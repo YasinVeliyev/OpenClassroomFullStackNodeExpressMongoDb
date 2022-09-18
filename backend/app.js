b70835bef547cf63d58e3fd51a86b3b1b6c0b43e
YasinVeliyev <veliyev.yasin@gmail.com>
2022-09-18 20:26:35 +0400

    new file:   RestApi/create.rest 	modified:   app.js 	new file:   models/thingsSchema.js 	modified:   package-lock.json 	modified:   package.json

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-<<<<<<< HEAD
 require("dotenv").config({ path: "../.env" });
 const mongoose = require("mongoose");
 const Thing = require("./models/thingsSchema");
-
 mongoose
     .connect(process.env.DATABASE_MONGODB_URI)
     .then(() => {
@@ -13,46 +11,7 @@ mongoose
         console.log("Unable to connect to MongoDB Atlas!");
         console.error(error);
     });
-<<<<<<< HEAD
-=======
-const express=require('express')
->>>>>>> parent of bc3147e (	modified:   backend/app.js)
-=======
->>>>>>> parent of d1cbe3d (Create a RESTful web API using Node, Express and MongoDB)
-=======
->>>>>>> parent of 54cc25c (Revert "	modified:   backend/app.js")
-
 const app = express();
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-const stuff = [
-    {
-        _id: "123456",
-        title: "My first Thing",
-        description: "All of about my things",
-        imageUrl: "",
-        price: 4900,
-        userId: "23111111111",
-    },
-    {
-        _id: "12345ass6",
-        title: "My second thing",
-        description: "All of about my things",
-        imageUrl: "",
-        price: 3900,
-        userId: "2311111s1111",
-    },
-];
-
->>>>>>> parent of d1cbe3d (Create a RESTful web API using Node, Express and MongoDB)
-=======
->>>>>>> parent of 54cc25c (Revert "	modified:   backend/app.js")
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -62,14 +21,59 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/stuff", (req, res, next) => {
-    res.status(200).json(stuff);
+app.get("/api/stuff", async (req, res, next) => {
+    try {
+        let things = await Thing.find();
+        console.log(things);
+        res.status(200).json(things);
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 });
 
-app.post("/api/stuff", (req, res, next) => {
-    stuff.push(req.body);
-    res.status(201).json({ message: "Things saved successfully", thing: req.body });
+app.get("/api/stuff/:thingId", async (req, res, next) => {
+    try {
+        let thing = await Thing.findById(req.params.thingId);
+        res.status(200).json(thing);
+    } catch (error) {
+        res.status(404).json({ error });
+    }
+});
+
+app.put("/api/stuff/:thingId", async (req, res, next) => {
+    let { title, description, imageUrl, userId, price } = req.body;
+    try {
+        let thing = await Thing.findByIdAndUpdate(
+            req.params.thingId,
+            { title, description, imageUrl, userId, price },
+            { new: true }
+        );
+        res.status(200).json(thing);
+    } catch (error) {
+        res.status(404).json({ error });
+    }
+});
+
+app.delete("/api/stuff/:thingId", async (req, res, next) => {
+    try {
+        await Thing.findByIdAndDelete(req.params.thingId);
+        res.status(200).json({ message: "Deleted" });
+    } catch (error) {
+        res.status(404).json({ error });
+    }
+});
+
+app.post("/api/stuff", async (req, res, next) => {
+    let { title, description, imageUrl, userId, price } = req.body;
+    try {
+        let thing = await Thing.create({ title, description, imageUrl, userId, price });
+        res.status(201).json({ message: "Things saved successfully", thing });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
 });
 
 module.exports = app;
